c159d0a38967285365d0509c1cea413f74656f18
Dominik Stenka <info@stenka.tech>
2023-09-15 19:55:49 +0200

    Initial Checkin

---
+++ CleanArchitecture/CleanArchitecture.Application/Features/UserFeatures/GetAllUser/GetAllUserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchitecture.Application.Features.UserFeatures.GetAllUser;
+
+public sealed record GetAllUserResponse
+{
+    public Guid Id { get; set; }
+    public string Email { get; set; } = default!;
+    public string Name { get; set; } = default!;
+}
---
+++ CleanArchitecture/CleanArchitecture.Domain/Entities/User.cs
@@ -0,0 +1,9 @@
+﻿using CleanArchitecture.Domain.Common;
+
+namespace CleanArchitecture.Domain.Entities;
+
+public class User : BaseEntity
+{
+    public string Email { get; set; } = default!;
+    public string Name { get; set; } = default!;
+}
